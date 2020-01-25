400bd1dd276f8fe307ef6577b88a738fe810954f
Rashad <rashad.walcott15@gmail.com>
2020-01-24 20:37:40 -0500

    Commenting

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -9,6 +9,7 @@ export class HttpService {
   constructor(private http: HttpClient) { }
 
   getBeer() {
+    //This is the get request for the information back from the API
     return this.http.get('https://api.openbrewerydb.org/breweries')
     
   }
